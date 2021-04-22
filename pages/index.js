8f7b934d2c2f0014465eb6e7151ac94ab73a2b19
Irvin Meneses <developer@irvinemmanuel.xyz>
2021-04-21 21:45:29 -0500

    Se inicio la creacion del compoente Home junto con el uso de Sass

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,107 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div>
+      <header className={styles.header}>
+        <div className={styles.logo} onClick={() => router.push('/')}>
+          <Image
+            src='https://ik.imagekit.io/yw2w0b4ajqv/text-it/logos/logo_G6W7KZC74.svg'
+            layout='responsive'
+            width={25}
+            height={27}
+          />
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <Link href='/login'>
+          <a className={styles.login}>Login</a>
+        </Link>
+      </header>
+      <section className={styles.hero}>
+        <h1>
+          Text<span className={styles.hero.h1?.span}>It</span>
+        </h1>
+        <p>
+          Meet people, contact your friends or do business, in an easy and
+          simple way.
+        </p>
+        <button>Create an account</button>
+        <Link href='/login'>
+          <a
+            className={styles.login}
+            style={{ fontSize: '16px', marginTop: '5px' }}
+          >
+            Login
+          </a>
+        </Link>
+      </section>
+      <div className={styles.mockup}>
+        <Image
+          src='https://ik.imagekit.io/yw2w0b4ajqv/text-it/general/mobile-mockup_zo4s5Wtey.svg'
+          width={461}
+          height={827}
+        />
+      </div>
+      <section className={styles['features-container']}>
+        <h1 className={styles['features-title']}>Express yourself!</h1>
+        <div className={styles.features}>
+          <div className={`${styles['feature-card']} ${styles.orange}`}>
+            <h3>Show your emotions</h3>
+            <p>Use our emojis or send images</p>
+            <div>
+              <Image
+                src='https://ik.imagekit.io/yw2w0b4ajqv/text-it/general/card-emojis_bHXf-IPHV.svg'
+                width={140}
+                height={123}
+              />
+            </div>
+          </div>
+          <div className={`${styles['feature-card']} ${styles.blue}`}>
+            <h3>Your privacy is primordial</h3>
+            <p>
+              Your messages are encryptedend to end, in addition, TextIt is open
+              source, everyone can know how TextIt works!
+            </p>
+            <div>
+              <Image
+                src='https://ik.imagekit.io/yw2w0b4ajqv/text-it/general/features-images-2_WQGrx3dQeL.svg'
+                width={217}
+                height={137}
+              />
+            </div>
+          </div>
+          <div className={`${styles['feature-card']} ${styles.orange}`}>
+            <h3>Personalize your profile</h3>
+            <p>Introduce yourself as you want</p>
+            <div>
+              <Image
+                src='https://ik.imagekit.io/yw2w0b4ajqv/text-it/general/features-images-3_bO4xniDaEt.svg'
+                width={156}
+                height={156}
+              />
+            </div>
+          </div>
+          <div className={`${styles['feature-card']} ${styles.blue}`}>
+            <h3>From your browser!</h3>
+            <p>
+              Text it works on any browser. You don't need to donwload an
+              instaler or extension
+            </p>
+            <div>
+              <Image
+                src='https://ik.imagekit.io/yw2w0b4ajqv/text-it/general/features-images-4_42fut9VBj.svg'
+                width={296}
+                height={253}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
